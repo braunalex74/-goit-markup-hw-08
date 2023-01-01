dcf6929c33a07e304bba67f97fd94822fd77dacd
braunalex74 <braunalex74@googlemail.com>
2023-01-01 21:02:44 +0200

    DZ_8

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
+  const refs = {
+    openmenuBtn: document.querySelector(".menu-btn"),
+    closemenuBtn: document.querySelector(".menu-close"),
+    menu: document.querySelector('.mobile-menu'),
+  };  
 
-    menuBtnRef.addEventListener("click", () => {
-      const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-      mobileMenuRef.classList.toggle("is-open");
-    });
+    refs.openmenuBtn.addEventListener('click', toggleMenu);
+    refs.closemenuBtn.addEventListener('click', toggleMenu);
+
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-hidden');
+    }
   })();
